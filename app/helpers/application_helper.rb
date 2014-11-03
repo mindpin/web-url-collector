4e1e0d4728304126fe4cd8156132cd8c5c13cf9c
fushang318 <fushang318@gmail.com>
2014-11-03 12:11:49 +0800

    move r.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,24 +53,4 @@ module ApplicationHelper
   def set_cookie!(user)
     cookies.permanent.signed[R::COOKIE_KEY] = user.id.to_s
   end
-
-  def find_by_token(token)
-    return if token.blank?
-    user = User.where(token: token).first
-    return user if !user.blank?
-    return create_user_user_by_info(token)
-  end
-
-  def user_info_url(value)
-    "#{R::USER_INFO_URL}?token=#{value}"
-  end
-
-  def create_user_user_by_info(token)
-    user_info = JSON.parse(RestClient.get(user_info_url(token)))
-    return if user_info["id"].blank?
-    user = User.find_or_create_by(id: user_info["id"])
-    user.update_attributes(name: user_info["name"], avatar: user_info["avatar"], uid: user_info["id"], token: user_info["token"])
-    user.save
-    user
-  end
 end
