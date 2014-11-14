68ecf9d4e0c44b7084d58150b6d94f4774b2a779
Kaid <kaid@kaid.me>
2014-11-14 18:39:34 +0800

    ImageUploader logging

--- app/workers/image_uploader.rb
+++ app/workers/image_uploader.rb
@@ -3,11 +3,13 @@ class ImageUploader
   sidekiq_options :queue => :image_uploader, :retry => false
 
   def perform(info_id, image)
+    logger.info "ImageUploader start at: #{Time.now}"
     info = UrlInfo.find(info_id)
 
     response  = Net::HTTP.post_form(URI("http://img.4ye.me/images"), :base64 => image)
     image_url = JSON.parse(response.body)["url"]
 
     info.update_attributes(:image_url => image_url)
+    logger.info "ImageUploader finish at: #{Time.now}"
   end
 end
